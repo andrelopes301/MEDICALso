4883ede06bdaeb804575389adddba40947799550
André Lopes <51788096+andrelopes301@users.noreply.github.com>
2021-11-27 15:20:38 +0000

    Update medico.h

--- medico.h
+++ medico.h
@@ -6,8 +6,8 @@ typedef struct medico Medico, *pMedico;
 struct medico{
 
     char nome[30];
-    char areaEspecialidade[100];
-    int id_Medico; // 0 - medico nao existe
+    char especialidade[100];
+    int id;// 0 - medico nao existe
             // > 0, - medico existe e o id corresponde ao seu pid
     int estado; // 0 - parado
                 // 1 - a trabalhar
