ecf53722cbc5919caee344ad583ab88f1d4d427d
André Lopes <51788096+andrelopes301@users.noreply.github.com>
2021-11-27 15:18:59 +0000

    Update medico.h

--- medico.h
+++ medico.h
@@ -1,17 +1,18 @@
 #ifndef TP_MEDICO_H
 #define TP_MEDICO_H
 
-#include "balcao.h"
 
-typedef struct medico Medico;
+typedef struct medico Medico, *pMedico;
 struct medico{
 
     char nome[30];
-    char especialidade[100];
-    int id_Medico;
+    char areaEspecialidade[100];
+    int id_Medico; // 0 - medico nao existe
+            // > 0, - medico existe e o id corresponde ao seu pid
     int estado; // 0 - parado
-                //1 - a trabalhar
+                // 1 - a trabalhar
 };
+
 
 #endif //TP_MEDICO_H
 
